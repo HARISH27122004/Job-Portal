df037813afeea58f32aef3f96148a58961dbbd6b
harish <harishcr320@gmail.com>
2025-09-18 15:20:41 +0530

    new change

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: [
     'http://localhost:5173', 
+    'https://new-job-portal-seven.vercel.app'
   ]
 }))
 app.use(express.json())
